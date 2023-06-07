dcdb5f498992a7acf27dc32c30948bb9049787bf
Theo Haas <theohaas@Mbiopc274.mikrobio.uni-frankfurt.de>
2023-06-07 14:24:59 +0200

    added if command to check for .gbk files in the folder

--- processing_CDS_in_range.py
+++ processing_CDS_in_range.py
@@ -3,7 +3,7 @@ import os
 import argparse
 
 # Set path to the dataset directory and define BGC typs and enzyme names + gene kind to search for
-path = '/Users/theohaas/Desktop/MBW_Master/2._Semester/Synthese_Naturstoffe/datasets/probedata/'
+path = '/Users/theohaas/Desktop/MBW_Master/2._Semester/Synthese_Naturstoffe/datasets/RiPP_antismash_out_genbank_files_out_full_analysis/'
 BGC_types = ["NRPS", "PKS"]
 enzyme_names = ["ycao", "ycaO", "YCAO", "P450", "p450", "radical SAM", "methyltransferase"]
 gene_kind = "biosynthetic"
@@ -29,42 +29,43 @@ for BGC_type in BGC_types:
 
         # Iterate over files in the dataset directory
         for progenome_file in os.listdir(final_path):
-            try:
-                # Parse the GenBank file
-                for seq_record in SeqIO.parse(final_path + progenome_file, "gb"):
-                    
-                    # Get all CDS features in the sequence record
-                    cds_features = [feature for feature in seq_record.features if feature.type == "CDS"]
-                    enzyme_sequences = []
+            if progenome_file.endswith(".gbk"):
+                try:
+                    # Parse the GenBank file
+                    for seq_record in SeqIO.parse(final_path + progenome_file, "gb"):
 
-                    # Iterate over the CDS features
-                    for i, seq_feature in enumerate(cds_features):
-                        # Check if the gene_kind is "biosynthetic"
-                        if seq_feature.qualifiers.get("gene_kind", [""])[0] == gene_kind:
-                            # Check surrounding CDS within the specified range
-                            start_index = max(i - num_cds_before, 0)
-                            end_index = min(i + num_cds_after + 1, len(cds_features))
+                        # Get all CDS features in the sequence record
+                        cds_features = [feature for feature in seq_record.features if feature.type == "CDS"]
+                        enzyme_sequences = []
 
-                            # Iterate over nearby CDS features
-                            for nearby_feature in cds_features[start_index:end_index]:
-                                # Check if the enzyme name is present in the nearby feature
-                                if enzyme_name.lower() in str(nearby_feature).lower():
-                                    assert len(nearby_feature.qualifiers['translation']) == 1
-                                    # Generate the enzyme sequence entry
-                                    enzyme_sequence = (
-                                        ">%s from %s (%s)\n%s\n" % (
-                                            nearby_feature.qualifiers['locus_tag'][0],
-                                            seq_record.name,
-                                            nearby_feature.qualifiers["product"],
-                                            nearby_feature.qualifiers['translation'][0]))
-                                    enzyme_sequences.append(enzyme_sequence)
+                        # Iterate over the CDS features
+                        for i, seq_feature in enumerate(cds_features):
+                            # Check if the gene_kind is "biosynthetic"
+                            if seq_feature.qualifiers.get("gene_kind", [""])[0] == gene_kind:
+                                # Check surrounding CDS within the specified range
+                                start_index = max(i - num_cds_before, 0)
+                                end_index = min(i + num_cds_after + 1, len(cds_features))
 
-                    # Write the enzyme sequences to the output file if any were found
-                    if enzyme_sequences:
-                        output_path.write('\n'.join(enzyme_sequences))
+                                # Iterate over nearby CDS features
+                                for nearby_feature in cds_features[start_index:end_index]:
+                                    # Check if the enzyme name is present in the nearby feature
+                                    if enzyme_name.lower() in str(nearby_feature).lower():
+                                        assert len(nearby_feature.qualifiers['translation']) == 1
+                                        # Generate the enzyme sequence entry
+                                        enzyme_sequence = (
+                                            ">%s from %s (%s)\n%s\n" % (
+                                                nearby_feature.qualifiers['locus_tag'][0],
+                                                seq_record.name,
+                                                nearby_feature.qualifiers["product"],
+                                                nearby_feature.qualifiers['translation'][0]))
+                                        enzyme_sequences.append(enzyme_sequence)
 
-            except Exception as e:
-                print("Error:", e)
+                        # Write the enzyme sequences to the output file if any were found
+                        if enzyme_sequences:
+                            output_path.write('\n'.join(enzyme_sequences))
+
+                except Exception as e:
+                    print("Error:", e)
 
         # Close the output file
         output_path.close()
